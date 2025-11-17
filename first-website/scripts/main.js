7133c8bb0da3891af0378e2f5cf30e254baaf268
Brokentoaster1999 <76240190+Brokentoaster1999@users.noreply.github.com>
2025-11-17 08:41:31 -0600

    Add files via upload

---
+++ first-website/scripts/main.js
@@ -0,0 +1,55 @@
+const myImage = document.querySelector("img")
+
+myImage.addEventListener("click", () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/main-power-rangers-logo.png") {
+        myImage.setAttribute("src", "images/power-rangers-logo.png");
+    } else {
+        myImage.setAttribute("src", "images/main-power-rangers-logo.png");
+    }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = 'Power Rangers progession list, ${myName}';
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Power Rangers progession list, ${storedName}`;
+}
+
+
+myButton.addEventListener("click", () => {
+    setUserName();
+})
+
+const listItems = document.querySelectorAll("li");
+
+function toggleDone(e) {
+  if (!e.target.className) {
+    e.target.className = "done";
+  } else {
+    e.target.className = "";
+  }
+}
+
+listItems.forEach((item) => {
+  item.addEventListener("click", toggleDone);
+});
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Power Rangers progession list, ${myName}`;
+  }
+}
